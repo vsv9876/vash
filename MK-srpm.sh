cf4d7c3b7a52405b811be459ad6d139aeae02fd4
Sergey Vovk <s.vovk>
2024-03-09 23:31:53 +0300

    v2.0.25 begin

--- MK-srpm.sh
+++ MK-srpm.sh
@@ -14,6 +14,8 @@ mach=$(uname -m)
 SPECS="$rpmbuild/SPECS"
 SOURCES="$rpmbuild/SOURCES"
 RPMS="$rpmbuild/RPMS"
+
+cd `/bin/pwd`
 pwd=$(pwd)
 
 conf=./BLD.cfg
@@ -41,31 +43,30 @@ esac
 version=`cat ./VERSION`
 pkgname="$prog-$version"
 
-#prepare source tar.gz
-#if [ ! -s $conf ];then
-#    echo STOP... $conf not exists or empty
-#fi
-
 make distclean
-set -x
+#set -x
 
 # pkgname actually is symlink to sources dir
 if [ -s $where/$pkgname ]; then
     rm -f $where/$pkgname
 fi
-ln -s `pwd` $where/$pkgname && \
-( cd $where && tar cfz "$pkgname.tar.gz" $pkgname/* ) \
- && rm -f $where/$pkgname
+ln -s $pwd $where/$pkgname && \
+( cd $where && tar czf "$pkgname.tar.gz" $pkgname/* ) \
+&& rm -f $where/$pkgname
 
-# make a spec (not yet) then build src.rpm
+
+# make a spec from template then build src.rpm
 
 # prepare spec from template
+#src_specfile=$(find $where -name $spec)
 src_specfile=$(find . -name $spec)
 echo src_specfile=$src_specfile
+#
 specfile=$(basename $src_specfile)
+#
 rm -f $SPECS/$specfile || exit
 cat $src_specfile | sed "s/^Version:.*\$/Version:    $version/g" \
->$SPECS/$specfile || exit
+> $SPECS/$specfile || exit
 
 ##tar czf "$pkgname.tar.gz" $pkgname/*
 
